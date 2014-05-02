5eac4f4b024186a69b28df4ad3eeb81f4dc789f3
Thomas Weinert <weinert@dimensional.de>
2014-05-02 15:17:13 +0200

    # Initial Commit, separate module from cms/framework repository

---
+++ src/_classmap.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+  'image_captcha' => '/image_captcha.php',
+  'image_sinus_distortion_captcha' => '/image_sinus_distortion_captcha.php',
+  'image_speaking_captcha' => '/image_speaking_captcha.php',
+  'wavsampler' => '/wav_sampler.php',
+);
